7da98e34b57449de5f9bc33e832c12d610e9e15c
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-02 21:36:03 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,8 +42,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   sorted_matrix = [ 
-    ["Oranges", "Pineapple", "Watermelon"],
-     ["Grapes", "Potatoes", "Strawberries"] 
+     
     ]
     sorted_matrix[1][1]
   
