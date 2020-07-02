8cb79771f067ed401308ef606da8d3da759adc8f
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-02 22:00:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -57,7 +57,7 @@ def matrix_update(matrix, row, column, new_value)
   # Return the updated matrix
   sorted_matrix = [ 
     
-     ["Grapefruit", "Lemons", "Watermelon", "Eggplant"],
+     ["Grapefruit", "Pineapple", "Lemoons", "Watermelon", "Eggplant"],
     ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"] 
   ]
   sorted_matrix[1][3] = "new_value"
